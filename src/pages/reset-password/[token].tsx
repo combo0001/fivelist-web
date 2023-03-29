a6b25bacaa23e386e3e36dcb24737c9b08ffdc09
combo <83129142+combo0001@users.noreply.github.com>
2023-03-29 12:51:41 -0300

    EDIT resetpassword

--- src/pages/reset-password/[token].tsx
+++ src/pages/reset-password/[token].tsx
@@ -1,12 +1,7 @@
 import { useRouter } from "next/router";
 
 export default function ResetPassword(): JSX.Element {
-    const { query } = useRouter();
-
     return (
-        <>
-            <pre>{query.token}</pre>
-            <h1>Hello world!</h1>
-        </>
+        <h1>Hello world!</h1>
     )
 }
