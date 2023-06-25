1250c2f405bff00cf94eabce4425050370e774c2
combo0001 <gabrielfleig04@gmail.com>
2023-06-25 00:59:19 -0300

    ADD component de edição de users: LInks

---
+++ src/pages/users/[custom_id]/edit/components/Links.tsx
@@ -0,0 +1,81 @@
+import { LinkDialog } from '@/components/Dialogs/Link'
+import { AddLink } from '@/components/EditLinks'
+import { WorldIcon } from '@/components/Icons'
+import { styled } from '@/styles'
+import { Button, Heading, Text } from '@5list-design-system/react'
+
+/* eslint-disable no-undef */
+interface LinksProps {
+  title: string
+  links: { label: string; url: string }[]
+}
+
+const LinksContainer = styled('div', {
+  minHeight: '9.25rem',
+
+  padding: '$6',
+
+  background: 'transparent',
+  border: '0.0625rem solid $neutral800',
+  borderRadius: '$lg',
+
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$6',
+})
+
+const LinksList = styled('ul', {
+  display: 'flex',
+  flexWrap: 'wrap',
+  alignItems: 'center',
+  gap: '$4',
+
+  '& > *': {
+    flex: '1 0 34%',
+    listStyleType: 'none',
+  },
+})
+
+const WebsiteLinkBox = styled('a', {
+  textDecoration: 'none',
+
+  cursor: 'pointer',
+
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$3',
+})
+
+export const Links = ({ title, links }: LinksProps): JSX.Element => {
+  return (
+    <LinksContainer>
+      <Heading as={'h4'} weight={'bold'}>
+        {title}
+      </Heading>
+
+      <LinksList>
+        {links.map(({ label, url }, index) => (
+          <WebsiteLinkBox href={url} target={'_blank'} key={index}>
+            <Button variation={'icon'} size={'sm'}>
+              <WorldIcon css={{ fill: '$white', size: '$6' }} />
+            </Button>
+
+            <Text
+              size={'sm'}
+              color={'$white'}
+              weight={'bold'}
+              css={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
+            >
+              {label}
+            </Text>
+          </WebsiteLinkBox>
+        ))}
+
+        <LinkDialog
+          title={`Adicionar ${title.toLocaleLowerCase()}`}
+          trigger={<AddLink text={`Adicionar ${title.toLocaleLowerCase()}`} />}
+        />
+      </LinksList>
+    </LinksContainer>
+  )
+}
